674b43d4c8f121cd90bc7970ad965b0407ea0611
Azazek6 <naturaespinola12@gmail.com>
2024-03-05 09:27:19 -0500

    fixes

--- src/scrapping/liveTab.js
+++ src/scrapping/liveTab.js
@@ -1,5 +1,5 @@
-import puppeteer from "puppeteer";
 import { credentials } from "../configurations/credentials.js";
+import { getDataForScrap } from "../utils/getData.js";
 
 export const startLive = async (openBrowser) => {
   const browser = await openBrowser;
@@ -27,92 +27,7 @@ export const startLive = async (openBrowser) => {
   );
 
   // Obtener datos
-  let datos = await page.evaluate(() => {
-    const arbsListElement = document.querySelector("ul.arbs-list");
+  let collection = await getDataForScrap(page)
 
-    if (!arbsListElement) {
-      console.error("No se encontró el elemento ul.arbs-list");
-      return null;
-    }
-
-    const data = [];
-
-    arbsListElement.querySelectorAll("li").forEach((arbItem) => {
-      const headerElement = arbItem.querySelector(".header");
-      const wrapperElements = arbItem.querySelectorAll(".bet-wrapper");
-
-      if (headerElement && wrapperElements.length > 0) {
-        const percent = headerElement
-          .querySelector(".percent")
-          .innerText.trim();
-        const sportName = headerElement
-          .querySelector(".sport-name")
-          .innerText.trim();
-        const time = headerElement
-          .querySelector(".updated-at")
-          .innerText.trim();
-
-        const sections = [];
-
-        wrapperElements.forEach((wrapperElement, index) => {
-          const book_name = wrapperElement
-            .querySelector(".bookmaker-name > span.text-ellipsis")
-            .innerText.trim();
-          const score = wrapperElement
-            .querySelector(".bookmaker-name > .current-score")
-            .innerText.trim();
-
-          const event_name = wrapperElement
-            .querySelector(".event-name > .name a")
-            .innerText.trim();
-
-          const league_name = wrapperElement
-            .querySelector(".event-name > .league")
-            .innerText.trim();
-
-          const market = wrapperElement
-            .querySelector(".market > span > span")
-            .innerText.trim();
-
-          const odds = wrapperElement
-            .querySelector("span.coefficient")
-            .innerText.trim();
-
-          const arrowUpElement = wrapperElement.querySelector(
-            ".odds-info .icomoon-arrow-up"
-          );
-          const arrowDownElement = wrapperElement.querySelector(
-            ".odds-info .icomoon-arrow-down"
-          );
-
-          // Obtener el nombre de la clase
-          const arrowClass = arrowUpElement
-            ? "icomoon-arrow-up"
-            : arrowDownElement
-            ? "icomoon-arrow-down"
-            : null;
-
-          sections.push({
-            book_name,
-            score,
-            event_name,
-            league_name,
-            market,
-            arrowClass,
-            odds,
-          });
-        });
-
-        // Puedes agregar más propiedades según tus necesidades
-        data.push({
-          header: { percent, sportName, time },
-          sections,
-        });
-      }
-    });
-
-    return data;
-  });
-
-  console.log(JSON.stringify(datos, null, 2));
+  console.log(JSON.stringify(collection, null, 2));
 };
---
+++ src/utils/getData.js
@@ -0,0 +1,98 @@
+export const getDataForScrap = async (page)=>{
+  let datos = await page.evaluate(() => {
+    const arbsListElement = document.querySelector("ul.arbs-list");
+
+    if (!arbsListElement) {
+      console.error("No se encontró el elemento ul.arbs-list");
+      return null;
+    }
+
+    const data = [];
+
+    arbsListElement.querySelectorAll("li").forEach((arbItem) => {
+      const headerElement = arbItem.querySelector(".header");
+      const wrapperElements = arbItem.querySelectorAll(".bet-wrapper");
+
+      if (headerElement && wrapperElements.length > 0) {
+        const percent = headerElement
+          .querySelector(".percent")
+          .innerText.trim();
+        const sportName = headerElement
+          .querySelector(".sport-name")
+          .innerText.trim();
+        const period_game = headerElement.querySelector(
+          ".arb-game-period > span"
+        );
+        const time = headerElement
+          .querySelector(".updated-at")
+          .innerText.trim();
+
+        const sections = [];
+
+        wrapperElements.forEach((wrapperElement, index) => {
+          const book_name = wrapperElement
+            .querySelector(".bookmaker-name > span.text-ellipsis")
+            .innerText.trim();
+          const score = wrapperElement
+            .querySelector(".bookmaker-name > .current-score")
+            .innerText.trim();
+
+          const event_name = wrapperElement
+            .querySelector(".event-name > .name a")
+            .innerText.trim();
+
+          const league_name = wrapperElement
+            .querySelector(".event-name > .league")
+            .innerText.trim();
+
+          const market = wrapperElement
+            .querySelector(".market > span > span")
+            .innerText.trim();
+
+          const odds = wrapperElement
+            .querySelector("span.coefficient")
+            .innerText.trim();
+
+          const arrowUpElement = wrapperElement.querySelector(
+            ".odds-info .icomoon-arrow-up"
+          );
+          const arrowDownElement = wrapperElement.querySelector(
+            ".odds-info .icomoon-arrow-down"
+          );
+
+          // Obtener el nombre de la clase
+          const arrowClass = arrowUpElement
+            ? "icomoon-arrow-up"
+            : arrowDownElement
+            ? "icomoon-arrow-down"
+            : null;
+
+          sections.push({
+            book_name,
+            score,
+            event_name,
+            league_name,
+            market,
+            arrowClass,
+            odds,
+          });
+        });
+
+        // Puedes agregar más propiedades según tus necesidades
+        data.push({
+          header: {
+            percent,
+            sportName,
+            period: period_game ? period_game.innerText.trim() : null,
+            time,
+          },
+          sections,
+        });
+      }
+    });
+
+    return data;
+  });
+
+  return datos
+}
