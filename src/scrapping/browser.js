538374c55cdd5113660147905141966504c79190
Azazek6 <naturaespinola12@gmail.com>
2024-03-06 08:58:28 -0500

    deteccion

--- src/scrapping/browser.js
+++ src/scrapping/browser.js
@@ -1,17 +1,19 @@
-import puppeteer from "puppeteer";
+import puppeteer from "puppeteer-extra";
+import stealhPlugin from "puppeteer-extra-plugin-stealth";
 
 export const openBrowser = async () => {
   try {
-    const browser = await puppeteer.launch({
-    headless: false,
-    args: ['--no-sandbox', '--disable-setuid-sandbox'],
-    ignoreHTTPSErrors: true,
-    defaultViewport: null,
-  });
+    puppeteer.use(stealhPlugin());
 
-  return browser;
+    const browser = await puppeteer.launch({
+      headless: false,
+      args: ["--no-sandbox", "--disable-setuid-sandbox"],
+      ignoreHTTPSErrors: true,
+      defaultViewport: null,
+    });
+
+    return browser;
   } catch (error) {
-    console.log('Could not create a browser instance => : ', error);
+    console.log("Could not create a browser instance => : ", error);
   }
-  
 };
