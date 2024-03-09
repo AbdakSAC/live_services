ebc20c3d33833f480b8967ac67ae07d22fd9da7d
Azazek6 <naturaespinola12@gmail.com>
2024-03-09 09:19:59 -0500

    fix

--- src/configurations/general.js
+++ src/configurations/general.js
@@ -2,7 +2,7 @@ export const PORT = 4010;
 
 const api_access = {
   development: "http://localhost:4090/api",
-  production: "https://api.surebet.tmoneysport.com/api",
+  production: "http://api.surebet.tmoneysport.com/api",
 };
 
 export const API_URL = api_access.development;
